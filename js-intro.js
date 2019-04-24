ac5af321b697a213c5fbb034a0c80edbdaab4752
Sebastian White <sebastian.swhite@gmail.com>
2019-04-24 15:05:42 -0700

    Finished assessment

--- js-intro.js
+++ js-intro.js
@@ -1,55 +1,114 @@
-// Intro to Javascript Assessments
-
-// Consider this variable:
-
-var mantra = "Be the dev";
-
-// 1. Write the code that returns 'B' from mantra.
-
-// 2. Write the code that determines if there is a 'x' in mantra.
-
-// 3. Write the code that determines if there is a 'v' in mantra.
-
-// Stretch: Write the code that returns the position of 'v' in mantra.
-
-
-// Consider the following statement:
-
-var codingIsEasy
-var imBored
-
-if (codingIsEasy && imBored) {
-    console.log("I need more!")
-} else {
-    console.log("Steady as she goes")
-}
-
-// 4. Update the codingIsEasy and imBored variables above the statement to the values required to log "I need more!"
-
-// Consider the following statement:
-
-var imLost
-var imFrustrated
-
-if (imLost || imFrustrated) {
-    console.log('Break time!')
-} else {
-    console.log('Keep coding!')
-}
-
-// 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
-
-// 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
-
-// Stretch: Using a while loop.
-
-// Super Stretch: Using forEach().
-
-// Consider the following variable:
-
-var message = "thisisateststring"
+// // Intro to Javascript Assessments
+// // var a = 5;
+// // var b = 10;
+// // console.log(`Fifteen is ${a + b} and
+// // not ${2 * a + b}.`);
+// // Consider this variable:
+//
+// var mantra = "Be the dev";
+//
+// // 1. Write the code that returns 'B' from mantra.
+// console.log(mantra.charAt(0));
+// // 2. Write the code that determines if there is a 'x' in mantra.
+// function txtSrchX(word){
+//   if(word.includes("x")){
+//     return `${word} includes x`
+//   }else{
+//     return `${word} does not include x`
+//   }
+// }
+// console.log(txtSrchX(mantra));
+// // 3. Write the code that determines if there is a 'v' in mantra.
+// function txtSrchV(word){
+//   if(word.includes("v")){
+//     return `${word} includes v`
+//   }else{
+//     return `${word} does not include v`
+//   }
+// }
+// console.log(txtSrchV(mantra));
+// // Stretch: Write the code that returns the position of 'v' in mantra.
+// function vSearch(word){
+//   let indexOfV = word.indexOf("v")
+//   return `letter v located at index ${indexOfV} of ${word}`
+// }
+// console.log(vSearch(mantra));
+// // Consider the following statement:
+//
+// var codingIsEasy = 5
+// var imBored = 5
+//
+// if (codingIsEasy && imBored) {
+//     console.log("I need more!")
+// } else {
+//     console.log("Steady as she goes")
+// }
+//
+// // 4. Update the codingIsEasy and imBored variables above the statement to the values required to log "I need more!"
+//
+// // Consider the following statement:
+//
+// var imLost = false
+// var imFrustrated = false
+//
+// if (imLost || imFrustrated) {
+//     console.log('Break time!')
+// } else {
+//     console.log('Keep coding!')
+// }
+//
+// // 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
+//
+// // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
+// function strIterate(word){
+//   for(let i = 0; i < word.length; i++){
+//     console.log((word.charAt(i)))
+//   }return "";
+// }
+// console.log(strIterate(mantra));
+// // Stretch: Using a while loop.
+// function strIterateWhile(word){
+//   let i = 0
+//   while(i <= word.length){
+//     console.log((word.charAt(i)));
+//     i++
+//   }return "";
+// }
+// console.log(strIterateWhile(mantra));
+//
+// // Super Stretch: Using forEach().
+// var wordArr = mantra.split("")
+//
+//
+//
+// function printToConsole(item){
+//   console.log(item)
+// }
+// (wordArr.forEach(printToConsole))
+// // Consider the following variable:
+//
+// var message = "thisisateststring"
 
 // 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+var dirtyWord = "this string has vowels"
+
+function vowelRemoval(string){
+  if( typeof string != "string"){
+    return "ERROR  a input is not string"
+  }
+
+  let stringArr = string.split("");
+  stringArr.forEach(vowelCheck)
+
+  function vowelCheck(item){
+    let vowels = ["a","e","i","o","u"]
+
+    if(vowels.includes(item)== false){
+    console.log(item)}
+  }
+  return "function returned"
+}
+console.log(vowelRemoval(dirtyWord));
 
 // Stretch: Update your function to throw an exception if the argument is not a string.
 
@@ -60,3 +119,12 @@ var message = "thisisateststring"
 var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
 
 // 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+function catFilter(animalArr){
+  let kittyCatArr = []
+  for(let i = 0; i< animalArr.length; i++){
+    if (animalArr[i].animal == "cat"){
+      kittyCatArr.push(animalArr[i].name)
+    }else{
+    }
+  }return kittyCatArr
+}console.log(catFilter(toonimals));
--- js-intro.md
+++ js-intro.md
@@ -3,13 +3,15 @@
 Answer these questions, then follow the instructions to submit your first assessment!
 
 #### 1. What is a function? Why would you use one?
+a function is a reusable block of code that takes in arguments and does something with them. a function would be used in cases where you want to run multiple variables or data sets through the same process. for example if you wanted to make a program that filtered a certain word or phrase out of an array you would use a function so that your filter could be called to filter multiple arrays rather than re-writing code and having each array hard coded in the filter.
 
 #### 2. What is a method?
-
+a method is a piece of code built into javascript that can take a certain data type and run it through a certain process depending on the method called. Methods vary based on data type, meaning each data type has its own unique methods though there are some exceptions of the same method being usable on multiple data types.
 #### 3. What are some examples of javascript string methods?
-
+for example, if you have a variable with the datatype of string you can use the string method "string.length()" to return the number of characters in any given string. Another example of a method is the ".forEach()" method which is used for arrays and will iterate through any given array and run each element through a specified function.
 #### 4. Explain the difference between accessor and mutator methods. Why would you use one over the other?
-
+an accessor is a method that accesses an array and returns a value or altered value from an array without changing the values in the original array. A mutator is a method that alters the original array instead of creating just pulling a value.
 #### 5. Explain the differences between console.log() and return. Why would you use one over the other?
-
+console.log is used to print information to the console log and a return is used to return data from a function to be used later in a program. console log is more useful for debugging and making sure your logic works in a function for example. a return would be used in a function to return usable data from a function
 #### 6. What is one thing we did in class this week that helped you learn the most?  
+I really like how the information we learn is run through in a brief lecture format then re-iterated through challenges and hands on project time 
